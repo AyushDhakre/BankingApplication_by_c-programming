49b383347e4274fd8a62f768bae2f9b7ff3de595
AyushDhakre <ayushdhakre8@gmail.com>
2024-09-03 15:20:17 +0530

    Add project files.

---
+++ BankingAapplication/functionDec.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//user menu function
+void userMenu();
+void deposit();
+void withdraw();
+void checkBalance();
+void viewAccountDetails();
+void requestToCreateAccount();
+void requestToDeleteAccount();
+
+
+//admin menu function
+void adminMenu();
+void addNewAccount();
+void viewAllAccounts();
+void deleteAccount();
+
